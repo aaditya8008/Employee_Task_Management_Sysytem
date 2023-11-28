1b53f6dc206ee239a5c231dcdec6b525731be9c3
aaditya8008 <114769448+aaditya8008@users.noreply.github.com>
2023-11-28 15:28:06 +0530

    Second commit

--- choose.php
+++ choose.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Background Image Page</title>
+  <title>Choose</title>
   <!-- JQuery file -->
   <script src="JQuery/jquery-3.7.1.min.js"></script>  <!-- JQuery Imported-->
   <!-- Boot Strap file -->
